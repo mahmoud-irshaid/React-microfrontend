b28c9eab4dd96634ee558aba77a583d715093602
mahmoud-irshaid <mirshaid10@gmail.com>
2023-05-20 16:15:55 +0300

    Create about content

--- about/src/App.jsx
+++ about/src/App.jsx
@@ -1,14 +1,17 @@
-import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.scss";
-
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: about</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <div class="text-center">
+      <img
+        src="https://mdbcdn.b-cdn.net/img/new/avatars/8.webp"
+        class="rounded-full w-32 mb-4 mx-auto"
+        alt="Avatar"
+      />
+      <h5 class="text-xl font-medium leading-tight mb-2">John Doe</h5>
+      <p class="text-gray-500">Web designer</p>
+    </div>
   </div>
 );
+
 ReactDOM.render(<App />, document.getElementById("app"));
