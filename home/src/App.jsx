6a04e2789ebf1aa50f7cc17e2c486189a2dc857e
mahmoud-irshaid <mirshaid10@gmail.com>
2023-05-20 16:15:01 +0300

    import header and footer

--- home/src/App.jsx
+++ home/src/App.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
 import "./index.scss";
+import Header from "./components/Header"
+import Footer from "./components/Footer";
 
 const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>Name: home</div>
-    <div>Framework: react</div>
-    <div>Language: JavaScript</div>
-    <div>CSS: Tailwind</div>
+  <div className="text-3xl mx-auto max-w-6xl">
+    <Header />
+    <div className="my-10">
+      Home page Content
+    </div>
+    <Footer />
   </div>
 );
+
 ReactDOM.render(<App />, document.getElementById("app"));
